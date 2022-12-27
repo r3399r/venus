a1a0b979afb1254eafab7f78e8faa38fd6157519
Yue <yue@potentia.asia>
2022-12-27 15:40:16 +0800

    introduce LIFF

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,10 +3,13 @@ set -e
 
 env=$1
 project=venus
+subDomain=wedding
+domain=celestialstudio.net
 
 echo ====================================================================================
 echo env: $env
 echo project: $project
+echo domain: $subDomain.$domain
 echo ====================================================================================
 
 # echo execute db scripts...
@@ -23,14 +26,14 @@ cd ../backend
 npm i
 npm run pre:deploy
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway-singapore
-aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project --no-fail-on-empty-changeset --s3-bucket y-cf-midway-singapore
+aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway-singapore
 echo ====================================================================================
 
-# echo deploy frontend to S3...
-# cd ../frontend
-# npm ci
-# npm run pre:deploy
+echo deploy frontend to S3...
+cd ../frontend
+npm i
+npm run build
 # mkdir -p ./dist/doc
 # cp -R ../doc/index.* ./dist/doc
-# aws s3 sync ./dist s3://$project-$env --delete --cache-control no-cache
-# echo ====================================================================================
+aws s3 sync ./dist s3://$project-$env-y --delete --cache-control no-cache
+echo ====================================================================================
